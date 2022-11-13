d050c2bae9eabcb8972ba22b60d628ada8a343d9
Andre Hu <andrebhu@gmail.com>
2022-11-13 17:19:16 -0500

    add 5-17

--- dataset/src/bucket_download.py
+++ dataset/src/bucket_download.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 
+import time
 import requests
 
 url = "https://storage.googleapis.com/andrebhu-personal/stackoverflow_answers/"
 
 num_buckets = 116
-start = 5
+start = 18
 
 def main():
     for i in range(start, num_buckets):
         bucket_url = url + str(i).zfill(12)
         r = requests.get(bucket_url)
         open(f'{str(i).zfill(12)}.gz', 'wb').write(r.content)
-        print("Downloaded bucket", str(i))
+        print(time.ctime(), "Downloaded bucket", str(i))
 
 if __name__ == "__main__":
     main()
